2b5b23f513393b36e3d9c1f1843d09e6c52566ad
Jisu Lee <dlwltn0430@naver.com>
2023-03-09 01:20:11 +0900

    feat: 구글 로그인 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import * as React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GoogleLoginBtn from './pages/googleLogin';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          {/*<Route path="/" element={<SearchCafe />} />*/}
+          <Route path="/" element={<GoogleLoginBtn />} />
         </Routes> 
       </Router>
     </div>
