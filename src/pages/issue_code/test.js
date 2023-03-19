162227c063a9e3fc2d2442ed73a9b25422f87be5
Jisu Lee <dlwltn0430@naver.com>
2023-03-19 22:50:52 +0900

    feat: 코드 발급 기능 구현

--- src/pages/issue_code/test.js
+++ src/pages/issue_code/test.js
@@ -2,40 +2,44 @@ import {useState} from 'react'
 import axios from 'axios'
 
 export default function Test () {
+    const [keyword, setKeyword] = useState('');
+    const [state, setState] = useState('');
     const [code, setCode] = useState('');
 
-    const majors = ['정보컴퓨터공학부', '기계공학부', '항공우주공학과', '조선해양공학과']
-    const [selectedMajor, setSelectedMajor] = useState('')
+    const state_list = ['대학생', '대학원생', '휴학생', '신입생']
+    const [selectedState, setSelectedState] = useState('')
 
     // 생성된 코드 가져와서 div에 보여주기 (api 완성이 안돼서 일단 주석 처리)
-    /*function getCode(e) {
+    function getCode(e) {
         e.preventDefault();
+        // state와 keyword 백에 보내기
+        axios.post("http://127.0.0.1:8080/test/usercreate", 
+            {
+                state: state,
+                keyword: keyword
+            }, 
+            {headers: { "Content-Type": `application/json`}})
+            .then((response) => {
+                console.log(response)
+            }) .catch((error) => {
+                console.log(error)
+            })
+        
+            /*
         axios.get("{url}")
         .then(function(response) {
             console.log(response)
-            setCode(response)
+            setCode(response.data.code)
             //localStorage.setItem('code', code)
-            return (
-            //아래 코드가 실행 안된다면 {code} 대신 {response} 넣기
-            <div className='showCode'>
-                {code}
-            </div>
-            )
-        }) .catch(function(error) {
-            console.log(error)
-        })
+            
+            }) .catch(function(error) {
+                console.log(error)
+            })
+        */
+    }
 
-    }*/
-
-    const [userDto, setUserDto] = useState({
-        major: '',
-        keywords: ''
-      });
-
-    const { major, keywords } = userDto;
-    
-    // 사용자가 선택한 학과 추출하기
-    function handleMajor(e) {
+   // 사용자가 선택한 학과 추출하기
+    /*function handleMajor(e) {
         setSelectedMajor(e.target.value);
         console.log(e.target.value)
         setUserDto({
@@ -43,15 +47,23 @@ export default function Test () {
           major: e.target.value
         })
         //localStorage.setItem('major', major)
-      }
+      }*/
+
+    // 사용자의 state 추출하기
+    const handleState = (e) => {
+        e.preventDefault()
+        const getState = e.target.value
+        setSelectedState(getState)
+        if (getState === "대학생") setState(1)
+        else if (getState === "대학원생") setState(2)
+        else if (getState === "휴학생") setState(3)
+        else if (getState === "신입생") setState(4)
+        else if (getState.length === 0) alert("신분을 선택해주세요") 
+    }
 
     // 사용자가 추가한 키워드 추출하기
-    const onChange = (e) => {
-        const { value, name } = e.target;
-        setUserDto({
-          ...userDto,
-          [name]: value
-        })
+    const handleKeyword = (e) => {
+        setKeyword(e.target.value)
         //localStorage.setItem('keywords', keywords)
       };
 
@@ -80,17 +92,25 @@ export default function Test () {
     return (
         <div>
             <p>1. 처음 사용하는 경우</p>
-            <button>코드 생성</button> {/*getCode 함수 연결시키기 */}
             <div className='selectMajor'>
-                <select name={major} onChange={handleMajor} value={selectedMajor} className="major">
-                    {majors.map((major) => (
+                <select name={state} onChange={handleState} value={selectedState} className="major">
+                    {state_list.map((major) => (
                         <option value={major} key={major}> {major} </option>
                     ))}
                 </select>
             </div>
-            <input name='keywords' onChange={onChange} value={keywords} className="addKeyword"/>
+            <div>
+                <input name='keyword' onChange={handleKeyword} value={keyword} className="addKeyword"/>
+            </div>
+            <div>
+                <button onClick={getCode}>코드 생성</button> 
+            </div>
+            <div>
+                {code}
+            </div>
 
             <p>2. 이전에 코드 발급한 적이 있는 경우</p>
+            {/*
             <input name='code' onChange={onChange} value={code} className="addKeyword"/>
             <button>코드 입력</button> {/*insertCode 함수 연결시키기 */}
         </div>
