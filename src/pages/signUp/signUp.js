c25a1e305f1b6924b5e7e7993700c4f9eb8bc193
Jisu Lee <dlwltn0430@naver.com>
2023-04-26 15:37:38 +0900

    style: 회원가입 페이지 scss 완성

--- src/pages/signUp/signUp.js
+++ src/pages/signUp/signUp.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 import './signUp.scss'
 
@@ -10,7 +11,7 @@ export default function SignUp () {
     const [findQuesNum, setQuesNum] = useState('')
     const [findAnswer, setAnswer] = useState('')
     const [isChecked, setIsChecked] = useState(false);
-    const question_list = ['선택', '내가 다닌 초등학교는?', '인상 깊게 읽은 책 이름은?', '내 별명은?']
+    const question_list = ['Choose a question', '내가 다닌 초등학교는?', '인상 깊게 읽은 책 이름은?', '내 별명은?']
 
     console.log(email)
     console.log(password)
@@ -46,30 +47,37 @@ export default function SignUp () {
     return (
         <div className='signUp'>
             <div className='logo_container'>
-                <img src="/assets/img/logo.jpg"/>
-                <p>회원가입</p>
+                <Link to='/'>
+                    <img src="/assets/img/logo.jpg"/>
+                </Link>
             </div>
+            <div className='title_container'>회원가입</div>
+            
             <div className='form_container'>
                 <form onSubmit={handleSignup}>
                     <div className='enterEmail'>
                         <div><label htmlFor='email'>아이디</label></div>
-                        <input name='email' onChange={e => setEmail(e.target.value)} value={email} type="email" className="email"/>
+                        <input name='email' onChange={e => setEmail(e.target.value)} value={email} type="email" className="email"
+                        placeholder='Enter your email'/>
                     </div>
 
                     <div className='enterPW'>
                         <div><label htmlFor='password'>비밀번호</label></div>
-                        <input name='password' onChange={e => setPW(e.target.value)} value={password} className="password"/>
-                        <p>최소 6자리 이상</p>
+                        <input name='password' onChange={e => setPW(e.target.value)} value={password} className="password"
+                        placeholder='Enter your password'/>
+                        <div className='limitation'>최소 6자리 이상</div>
                     </div>
 
                     <div className='checkPW'>
                         <div><label htmlFor='check_password'>비밀번호 확인</label></div>
-                        <input name='checkedPassword' onChange={e => setPW2(e.target.value)} value={checkedPassword} className="password2"/>
+                        <input name='checkedPassword' onChange={e => setPW2(e.target.value)} value={checkedPassword} className="password2"
+                        placeholder='Check your password'/>
                     </div>
 
                     <div className='enterNickname'>
                         <div><label htmlFor='nickname'>닉네임</label></div>
-                        <input name='nickname' onChange={e => setNickname(e.target.value)} value={nickname} className="nickname"/>
+                        <input name='nickname' onChange={e => setNickname(e.target.value)} value={nickname} className="nickname"
+                        placeholder='Enter your nickname'/>
                     </div>
 
                     <div className='selectMajor'>
@@ -83,16 +91,21 @@ export default function SignUp () {
 
                     <div className='enterAnswer'>
                         <div><label htmlFor='findAnswer'>답</label></div>
-                        <input name='findAnswer' onChange={e => setAnswer(e.target.value)} value={findAnswer} className="findAnswer"/>
+                        <input name='findAnswer' onChange={e => setAnswer(e.target.value)} value={findAnswer} className="findAnswer"
+                        placeholder='Enter your answer'/>
                     </div>
 
                     <div className='agreement'>
-                        <input type="checkbox" checked={isChecked} onChange={e => setIsChecked(e.target.checked)} className="agreement"/>
-                        <label htmlFor='agreement'>이용약관 및 개인정보 취급방침에 동의합니다.</label>
+                        <input type="checkbox" checked={isChecked} onChange={e => setIsChecked(e.target.checked)}/>
+                        <label htmlFor='agreement_content'>이용약관 및 개인정보 취급방침에 동의합니다.</label>
                     </div>
 
                     <div>
                         <button type="submit">회원가입</button> 
+                    </div>
+
+                    <div className='link_to_login'>
+                        Already have an account? <Link to='/login'>Log in</Link>
                     </div>
                 </form>
             </div>
