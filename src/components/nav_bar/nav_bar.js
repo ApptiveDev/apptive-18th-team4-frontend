0fd6a0a542cb96546d612174e3943bb3f23348e1
Jisu Lee <dlwltn0430@naver.com>
2023-05-21 19:42:27 +0900

    style: navbar hover 효과 추가

--- src/components/nav_bar/nav_bar.js
+++ src/components/nav_bar/nav_bar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     return(
         <div className='nav_bar'>
             <div style={{padding: '0 14%'}} className='nav_bar'>
-                <img src='./assets/img/navbar_logo.png' className='logo'/>
+                <Link to="/"><img src='./assets/img/navbar_logo.png' className='logo'/></Link>
                 <Link to="/announcement">공지사항</Link>
                 <Link to="/nearBuilding">빈 강의실 찾기</Link>
                 <Link to="/annualPlan">학사일정</Link>
