7655b085ae5b673a98c0b7af5c0c88a4ab26bb8f
Jisu Lee <dlwltn0430@naver.com>
2023-06-01 12:59:28 +0900

    feat: 캘린더 모달창 구현

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -17,41 +17,57 @@ const Modal = ({selectedDate, selectedDayWeek}) => {
     const selected_day_week = day_week[selectedDayWeek];
 
     return (
-        <div style={{background: 'pink'}}>
+        <div className='modal_container'>
             <button onClick={openModal}>모달 열기</button>
         {isOpen && (
             <div className='modal'>
                 <div className="modal-content">
-                    <div className='close' onClick={closeModal}>
-                        &times;
+                    <div className='close_container' onClick={closeModal}>
+                        <div className='close'>&times;</div>
                     </div>
                     <div>
                         <input className='schedule_name' placeholder='일정 이름'/>
+                        <div style={{display: 'flex'}}>
                         <input className='start_date' placeholder='시작시간' value={`${selectedDate} ${selected_day_week}`} />
-                        {selected_day_week}
                         <input className='end_date' placeholder='종료시간'/>
+                        </div>
                         <input className='location' placeholder='장소'/>
                         <input className='memo' placeholder='메모'/>
                     </div>
-                    <div style={{border: '1px solid black'}}>
+                    <div className='time_container'>
                         <span>알림</span>
+
+                        <div style={{display: 'flex', justifyContent: 'flex-end', alignItems: 'center'}}>
+                        <div>
+                        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '7px'}}>
                         <button className='select_time'>10분 전</button>
                         <button className='select_time'>15분 전</button>
                         <button className='select_time'>30분 전</button>
+                        </div>
+                        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                         <button className='select_time'>1일 전</button>
                         <button className='select_time'>2일 전</button>
                         <button className='select_time'>1주일 전</button>
+                        </div>
+                        </div>
+
+                        </div>
+
                     </div>
                     <div className='color_container' style={{border: '1px solid black'}}>
                         <span>색상</span>
+
+                        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                         <div className='select_color' style={{backgroundColor: '#FF3737'}}/>
                         <div className='select_color' style={{backgroundColor: '#FFB571'}}/>
                         <div className='select_color' style={{backgroundColor: '#FFD910'}}/>
                         <div className='select_color' style={{backgroundColor: '#9BD3A1'}}/>
                         <div className='select_color' style={{backgroundColor: '#AEC0F0'}}/>
                         <div className='select_color' style={{backgroundColor: '#080038'}}/>
+                        </div>
+
                     </div>
-                    <div>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
                         <button className='add_schedule'>일정 추가하기</button>
                     </div>
                 </div>
