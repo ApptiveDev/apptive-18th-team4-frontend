7dbdf634d883e7aa5f273e2134a4e36755beff50
Jisu Lee <dlwltn0430@naver.com>
2023-07-05 19:30:41 +0900

    style: navbar css 수정 + 홈화면 top_container css

--- src/components/nav_bar/nav_bar.js
+++ src/components/nav_bar/nav_bar.js
@@ -6,11 +6,13 @@ export default function Navbar() {
         <div className='nav_bar'>
             <div style={{padding: '0 14%'}} className='nav_bar'>
                 <Link to="/"><img src='./assets/img/navbar_logo.png' className='logo'/></Link>
-                <Link to="/announcement">공지사항</Link>
                 <Link to="/nearBuilding">빈 강의실 찾기</Link>
                 <Link to="/annualPlan">학사일정</Link>
+                <Link to="/announcement">공지사항</Link>
                 <div className='button_container'>
-                    <button className='home'>Home</button>
+                    <Link to="/signup">
+                        <button className='home'>Sign Up</button>
+                    </Link>
                     <button className='logout'>Log Out</button>
                 </div>
             </div>
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,51 +3,89 @@ import './home.css'
 
 export default function Home() {
     return (
-        <div className="home_container">
-            <div className='top_container'>
-                <div className='top_container1'>
-                    <img src="/assets/img/logo_home.png" />
-                    <p>부산대 알리미</p>
-                </div>
-                
-                <div className='top_container2'>
-                    <Link to="/signUp">
-                        <button name="SignUp">SignUp</button>
-                    </Link>
-                    <Link to="/login">
-                        <button name="Login">Login</button>
-                    </Link>
-                </div>
-            </div>
-
-            <div className='middle_container'>
-                여러분의 소중한 시간을 지켜드리는<br/>부산대 알리미
-            </div>
-
-            <div className='bottom_container'>
-                <div className='notice'>
-                    <div className='bottom_container1'>
-                        <span>공지사항</span>
-                        <img src='/assets/img/plus_button.png' />
-                    </div>
-                </div>
-                <div className='annual_plan'>
-                    <div className='bottom_container1'>
-                        <span>학사일정</span>
-                        <Link to="/annualPlan">
-                            <img src='/assets/img/plus_button.png' />
+        <div>
+            <div className="top_container">
+                {/*상단 navbar*/}
+                <div style={{padding: '0 14%'}} className='home_nav'>
+                    <Link to="/"><img src='./assets/img/navbar_logo.png' className='logo'/></Link>
+                    <Link to="/nearBuilding">빈 강의실 찾기</Link>
+                    <Link to="/annualPlan">학사일정</Link>
+                    <Link to="/announcement">공지사항</Link>
+                    <div className='button_container'>
+                        <Link to="/signup">
+                            <button className='home'>Sign Up</button>
                         </Link>
-                        
+                        <button className='logout'>Log Out</button>
                     </div>
                 </div>
-                <div className='near_building'>
-                    <div className='bottom_container1'>
-                        <span>빈 강의실 찾기</span>
-                        <img src='/assets/img/plus_button.png' />
+                {/*상단 로고, 아이콘*/}
+                <div className="sub_container">
+                    <div style={{}}>
+                        <div style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            width: '7.125rem',
+                            height: '7.125rem',
+                            background: '#fff',
+                            borderRadius: '0.8125rem', 
+                        }}>
+                            <img src="/assets/img/logo_home2.png" />
+                        </div>
+
+                        <div style={{display: 'flex'}}>
+                            <div>
+                                <div style={{
+                                    width: '30rem',
+                                    color: '#13005E',
+                                    fontSize: '4rem',
+                                    fontStyle: 'normal',
+                                    fontWeight: '600',
+                                    padding: '2.44rem 0'
+                                }}>
+                                    부산대 알리미
+                                </div>
+                                <div style={{
+                                    color: '#FFF',
+                                    fontSize: '1.5rem'
+                                }}>
+                                    여러분의 소중한 시간을 지켜드리는
+                                </div>
+                            </div>
+                            <div style={{display: 'flex', alignItems: 'flex-end'}}>
+                                <a href='#s1'>
+                                    <div className='shortcutBtn'>
+                                        <img src="/assets/img/home_icon1.png" />
+                                        빈 강의실 찾기
+                                    </div>
+                                </a>
+                                <a href='#s2'>
+                                    <div className='shortcutBtn'>
+                                        <img src="/assets/img/home_icon2.png" />
+                                        학사일정
+                                    </div>
+                                </a>
+                                <a href='#s3'>
+                                    <div className='shortcutBtn'>
+                                        <img src="/assets/img/home_icon3.png" />
+                                        공지사항
+                                    </div>
+                                </a>
+                            </div>
+
+                        </div>
+
                     </div>
                 </div>
             </div>
-                
+            
+            <div id="s1">
+                <img src="/assets/img/home_icon1.png" style={{backgroundColor: 'black'}}/>
+                빈 강의실
+            </div>
+            <div id="s2">학사일정</div>
+            <div id="s3">공지사항</div>
+
         </div>
     )
 }
