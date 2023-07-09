6c341e9dc5814ace6084d93e76421c2f7878cbe2
Jisu Lee <101401447+dlwltn0430@users.noreply.github.com>
2023-07-09 13:55:15 +0900

    Revert "회원(로그인, 회원가입, 개인정보 찾기) 관련 부 리팩터링"

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -3,16 +3,10 @@ import {useNavigate, Link} from 'react-router-dom';
 import axios from 'axios';
 import './login.css';
 import GradationBox from '../../components/gradation_box/gradation_box';
-import api from '../../api';
 
 export default function Login() {
-
-    const navigate = useNavigate();
-
     const [email, setEmail] = useState('');
     const [password, setPW] = useState('');
-    const [rememberMe, setRememberMe] = useState(false);
-
 
     const getLoginInfo = () => {
         const accessToken = localStorage.getItem('accessToken')
@@ -30,47 +24,23 @@ export default function Login() {
         setRefreshToken(refreshToken);
     }, []);
 
-    useEffect(() => {
-        const savedUsername = localStorage.getItem('savedUsername');
-        if (savedUsername) {
-            setEmail(savedUsername);
-            setRememberMe(true);
-        }
-    }, []);
-  
-    
     const handleLogin = (e) => {
         e.preventDefault()
         if (email.length === 0) alert("아이디를 입력해주세요")
         else if (password.length === 0) alert("비밀번호를 입력해주세요")
         else {
-            api.post("/auth/login", {
+            axios.post("http://3.34.82.40:8080/auth/login", {
                     username: email, 
                     password: password
-                })
-            .then(onLoginSuccess) 
+                }, { headers: { 'Content-Type': 'application/json'} })
+            .then({onLoginSuccess}) 
             .catch(function(error) {
-                //401 코드 추가(네트워크 오류 고려)
-                if (error.response && error.response.status === 401) {
-                    alert('아이디 또는 비밀번호가 올바르지 않습니다.');
-                } else {
-                    console.log(error);
-                }
+                console.log(error)
             })
-            if (rememberMe) {
-                // rememberMe가 true일 경우 username을 로컬 스토리지에 저장
-                localStorage.setItem('savedUsername', email);
-            } else {
-                // rememberMe가 false일 경우 savedUsername 항목 삭제
-                localStorage.removeItem('savedUsername');
-            }
         }
-
     }
 
     function onSilentRefresh() {
-
-
         //local storage에 저장된 토큰 값 가져오기
         const accessToken = localStorage.getItem('accessToken');
         const refreshToken = localStorage.getItem('refreshToken');
@@ -82,26 +52,13 @@ export default function Login() {
             isRefreshing = true;
         }
     
-        api.post("/auth/reissue", {
+        axios.post("http://3.34.82.40:8080/auth/reissue", {
                   accessToken: accessToken,
                   refreshToken: refreshToken
-                })
+                }, { headers: { 'Content-Type': 'application/json'} })
         .then(onLoginSuccess) 
         .catch(function(error) {
-            // refreshToken이 만료되었거나 잘못된 경우 에러 처리
-            if (error.response && error.response.status === 401) {
-                
-                // refreshToken이 만료되었거나 유효하지 않음
-                // 로컬에서 토큰 삭제
-                localStorage.removeItem('accessToken');
-                localStorage.removeItem('refreshToken');
-
-                //세션 만료 알림
-                alert('세션이 만료되었습니다. 다시 로그인 해주세요.');
-                navigate('/login');
-            } else {
-                console.log(error);
-            }
+          console.log(error)
         })
         .finally(() => {
             // 갱신이 완료된 경우에만 다시 자동 갱신 실행
@@ -113,20 +70,15 @@ export default function Login() {
 
     onSilentRefresh.timeoutId = null;
 
-    //동적으로 받아오는 로직 작성해도 좋음
     const JWT_EXPIRY_TIME = 0.5 * 3600 * 1000; // 만료 시간 (30분 밀리 초로 표현)
 
-
-    //onLoginSuccess
     function onLoginSuccess(response) {
         const { accessToken, refreshToken } = response.data;
         // local storage에 at, rt 저장
         localStorage.setItem('accessToken', accessToken);
-        localStorage.setItem('refreshToken', refreshToken); 
+        localStorage.setItem('refreshToken', refreshToken);
         console.log(accessToken)
         console.log(refreshToken)
-
-        navigate('/');
 
         // accessToken 설정
         axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
@@ -160,10 +112,6 @@ export default function Login() {
 
                 <div style={{display: 'flex', justifyContent: 'center'}}>
                     <button onClick={handleLogin}>Log In</button> 
-                </div>
-                <div className='rememberMe'>
-                    <input type='checkbox' onChange={() => setRememberMe(!rememberMe)} checked={rememberMe} />
-                    <label>아이디 저장하기</label>
                 </div>
 
                 <div className='link_to_signUp' style={{marginLeft: '60px'}}>
