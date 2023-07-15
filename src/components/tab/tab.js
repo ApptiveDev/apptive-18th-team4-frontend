1fac2adcd8584963245674aff19eaa318c000a7d
Jisu Lee <dlwltn0430@naver.com>
2023-07-16 00:55:22 +0900

    rename: 빈 강의실 조건 설정 모달 컴포넌트 삭제 후 파일 병합

--- src/components/tab/tab.js
+++ src/components/tab/tab.js
@@ -14,6 +14,7 @@ function Tab() {
             <Nav justify variant="tabs">
                 <Nav.Item onClick={() => handleTabClick('tab1')}>
                     <Nav.Link active={selectedTab === 'tab1'} style={{ background: selectedTab === 'tab1' ? '#fff' : '#E2E2E2' }}>제6공학관</Nav.Link>
+                    
                 </Nav.Item>
                 <Nav.Item onClick={() => handleTabClick('tab2')}>
                     <Nav.Link active={selectedTab === 'tab2'} style={{ background: selectedTab === 'tab2' ? '#fff' : '#E2E2E2' }}>제7공학관</Nav.Link>
@@ -22,7 +23,26 @@ function Tab() {
                     <Nav.Link active={selectedTab === 'tab3'} style={{ background: selectedTab === 'tab3' ? '#fff' : '#E2E2E2' }}>제8공학관</Nav.Link>
                 </Nav.Item>
             </Nav>
+
+            {selectedTab === 'tab1' && <div style={{display: 'flex'}}>
+                <div style={{width: '50%', overflowY: 'scroll', height: '260px'}}>
+                    <div style={{display: 'flex', justifyContent: 'space-between', margin: '2rem 3rem'}}>
+                        <span>현재 빈 강의실 목록 </span>
+                        <span>사용 가능한 시간</span>
+                    </div>
+        
+                </div>
+
+                <div style={{width: '50%'}}>
+                    <div style={{display: 'flex', justifyContent: 'space-between', margin: '2rem 3rem'}}>
+                        <span>수업이 곧 끝나는 강의실 </span>
+                        <span>사용 가능한 시간</span>
+                    </div>
+                </div>
+            </div>}
+            
         </div>
+
     );
 }
   
--- src/pages/nearBuilding/nearBuilding.js
+++ src/pages/nearBuilding/nearBuilding.js
@@ -1,12 +1,44 @@
 import { useState, useEffect } from "react";
 import Navbar from "../../components/nav_bar/nav_bar";
-import Modal_nearBuilding from "../../components/modal_nearBuilding/modal";
 import Tab from "../../components/tab/tab";
 import Map from "../../components/map_nearBuilding/map";
 import './nearBuilding.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import data from './data.json';
+import useGeoLocation from "../geolocation/useGeoLocation";
+import { instance } from "../../components/ApiContoller";
 
 export default function NearBuilding() {
+    const [isLogin, setIsLogin] = useState(false);
+    const [selectedTime, setTime] = useState('');
+    const timeZones = ['0.5', '1', '2', '3'];
+
+    useEffect(() => {
+        if (localStorage.getItem('accessToken') !== null) {
+            setIsLogin(true)
+        }
+    }, [isLogin]);
+    
+    const location = useGeoLocation();
+    const [lat, setLat] = useState('');
+    const [lang, setLang] = useState('');
+
+    useEffect(() => {
+        setLat(location.coordinates.lat);
+        setLang(location.coordinates.lang);
+    }, [])
+    
+    let requestURL = '';
+    const handleResult = () => {
+        if (selectedTime === '') requestURL = `/api/nearest-buildings/byloc?user_latitude=${lat}&user_longitude=${lang}`;
+        else requestURL = `/api/nearest-buildings/byloc?user_latitude=${lat}&user_longitude=${lang}&user_settime=${selectedTime * 60}`;
+        instance.get(requestURL)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+    }
+
+    //console.log(data.slice(0, 3))
+
     return(
         <div style={{background: '#F9F9F9'}}>
             <Navbar />
@@ -17,7 +49,33 @@ export default function NearBuilding() {
                 }}>
                     빈 강의실 찾기
                 </span>    
-                <Modal_nearBuilding />
+                <div style={{marginTop: '3rem'}}>
+                    <span style={{marginRight: '2rem'}}>내 위치로 가져오기</span>
+                    {isLogin && <span>즐겨찾기로 가져오기</span>}
+                </div>
+                <div className="select-condition">
+                    {selectedTime !== '' ? `${selectedTime}시간 이상` : '조건을 설정해주세요'}
+                    {/*<img src="/assets/img/arrow_right.png"></img>*/}
+                </div>
+
+                <div className="modal_nearbuilding">
+                    <div className="container">
+                        <label>사용할 시간</label>
+                        {timeZones.map((timeZone) => (
+                            <div className="dayOfWeek"
+                                key={timeZone}
+                                onClick={() => setTime(timeZone)}>
+                                {timeZone}
+                            </div>
+                        ))}
+                        <label style={{marginLeft: '1.56rem'}}>시간 이상</label>
+                        <button className="finish"
+                            onClick={handleResult}>
+                            조회
+                        </button>
+                    </div>
+                </div>
+
                 <Tab />
                 <div style={{marginTop: '5rem'}}>
                     <button className="current-location">현재 위치</button>
