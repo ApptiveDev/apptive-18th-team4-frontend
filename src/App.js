e09ed5b0618a2ec986f7bec31bad7a6f49c69e8d
Jisu Lee <dlwltn0430@naver.com>
2023-07-28 02:26:07 +0900

    style: 로고 변경

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import * as React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './pages/home/home'
+//기능1
 import SignUp from './pages/signUp/signUp'
 import Login from './pages/login/login'
 import FindId from './pages/find_id/find_id';
 import FindPw from './pages/find_pw/find_pw';
-
+//기능2 (공지사항 조회)
 import Announcement from './pages/announcement/announcement';
+//기능3 (빈 강의실 조회)
 import AnnualPlan from './pages/annual_plan/annual_plan';
+//기능4 (학사일정 조회)
 import NearBuilding from './pages/nearBuilding/nearBuilding';
-
-import MessageComponent from './pages/message_test/firebase-message-sw';
-import Test from './pages/geolocation/index'
 
 function App() {
   return (
@@ -24,13 +24,9 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/findId" element={<FindId />} />
           <Route path="/findPw" element={<FindPw />} />
-
           <Route path="/announcement" element={<Announcement />} />
           <Route path='/annualPlan' element={<AnnualPlan />} />
           <Route path="/nearBuilding" element={<NearBuilding />} />
-
-          <Route path="/test" element={<Test />} />
-          <Route path="/msg" element={<MessageComponent />} />
         </Routes> 
       </Router>
     </div>
